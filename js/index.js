03c4406e9c22272e63f75720d43cc5ab38fe23cb
Filbog <f.boguslawski101@gmail.com>
2024-03-07 03:42:18 +0100

    adding all the files

---
+++ js/index.js
@@ -0,0 +1,185 @@
+// API request setup
+const APIKEY = "89f20445788ab5ad5af51b3231833937"; // my Last.fm API key
+const artist = "Led%20Zeppelin";
+const limit = 20;
+const url = `http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${artist}&api_key=${APIKEY}&format=json&limit=${limit}`;
+
+// Base album price for simplicity
+const BASE_PRICE = 49.0;
+
+// Fetch albums from API
+function fetchAlbums() {
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      displayAlbums(data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+// Display albums on the page
+function displayAlbums(data) {
+  const albums = data.topalbums.album;
+  const displayAlbums = document.getElementById("display-albums");
+  displayAlbums.innerHTML = ""; // clear the div
+
+  albums.forEach((album) => {
+    const albumDiv = document.createElement("div");
+    albumDiv.className =
+      "col-12 col-md-6 col-lg-4 col-xl-3 d-flex justify-content-center"; // Bootstrap grid classes
+
+    albumDiv.innerHTML = `
+      <div class="card m-2" style="width: 18rem;">
+        <img src="${album.image[2]["#text"]}" class="card-img-top album-image" alt="${album.name}">
+        <div class="card-body">
+          <h5 class="card-title">${album.name}</h5>
+        </div>
+        <div class="card-img-overlay d-flex align-items-center justify-content-center">
+        <button class="btn btn-primary buy-button px-3 py-2" data-bs-toggle="modal" data-bs-target="#purchaseModal" type="button" data-album="${album.name}">Buy now</button>
+      </div>
+      </div>
+    `;
+    displayAlbums.appendChild(albumDiv); // append the albumDiv to displayAlbums
+  });
+
+  // Add event listener to "Buy" buttons
+  document.querySelectorAll(".buy-button").forEach((button) => {
+    button.addEventListener("click", (event) => {
+      const albumName = event.target.dataset.album;
+      console.log(albumName);
+      document.getElementById("albumName").textContent = `${albumName}`;
+      // Update the rest of the modal content here
+      // new bootstrap.Modal(document.getElementById('purchaseModal')).show();
+    });
+  });
+}
+
+// Set max/min attributes for shipment date
+function setShipmentDateAttributes() {
+  // Select the input element
+  var shipmentDateInput = document.getElementById("shipmentDate");
+
+  // Calculate tomorrow's date and 14 days from now
+  var tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  var twoWeeksFromNow = new Date();
+  twoWeeksFromNow.setDate(twoWeeksFromNow.getDate() + 60);
+
+  // Format the dates in the 'YYYY-MM-DD' format
+  var minDate = tomorrow.toISOString().split("T")[0];
+  var maxDate = twoWeeksFromNow.toISOString().split("T")[0];
+
+  // Set the shipment date attrs
+  shipmentDateInput.setAttribute("min", minDate);
+  shipmentDateInput.setAttribute("max", maxDate);
+}
+
+// Update price when buying a particular album
+function updateTotalPrice() {
+  // Get the total price element
+  var totalPriceElement = document.getElementById("totalPrice");
+
+  // Get all the checkboxes
+  var checkboxes = document.querySelectorAll("input[type=checkbox]");
+
+  // Function to update the total price
+  function calculateTotalPrice() {
+    var totalPrice = BASE_PRICE;
+
+    // Add the price of each checked checkbox to the total price
+    checkboxes.forEach(function (checkbox) {
+      if (checkbox.checked) {
+        totalPrice += parseFloat(checkbox.value);
+      }
+    });
+
+    // Update the total price element
+    totalPriceElement.textContent =
+      "Total price: " + totalPrice.toFixed(2) + "$";
+  }
+
+  // Update the total price initially
+  calculateTotalPrice();
+
+  // Add event listeners to the checkboxes
+  checkboxes.forEach(function (checkbox) {
+    checkbox.addEventListener("change", calculateTotalPrice);
+  });
+}
+
+// Handle form submission
+function handleFormSubmission() {
+  // Get the form
+  var form = document.getElementById("purchaseForm");
+
+  // Add an event listener for the form
+  form.addEventListener("submit", function (event) {
+    // Prevent the default form submission
+    event.preventDefault();
+
+    // Get the form data
+    var formData = new FormData(form);
+
+    // Create an object to hold the form data
+    var data = {};
+    formData.forEach(function (value, key) {
+      data[key] = value;
+    });
+
+    // get album name and add it to the data object
+    let albumName = document.getElementById("albumName").textContent;
+    data["albumName"] = albumName;
+
+    displaySummary(data);
+
+    const hiddenModalToggler = document.getElementById("hiddenModalToggler");
+    hiddenModalToggler.click();
+    // Log the form data
+    console.log(data);
+  });
+}
+
+// Display the summary
+function displaySummary(data) {
+  // Create the summary string
+  var summary =
+    "Album Name: " +
+    data["albumName"] +
+    "\n" +
+    "Name: " +
+    data["name"] +
+    "\n" +
+    "Last Name: " +
+    data["last_name"] +
+    "\n" +
+    "Address: " +
+    data["address"] +
+    "\n" +
+    "Email: " +
+    data["email"] +
+    "\n" +
+    "Payment Option: " +
+    data["payment"] +
+    "\n" +
+    "Shipment Date: " +
+    data["shipmentDate"];
+
+  // Insert the summary into the modal body
+  document.querySelector("#summaryModal .modal-body").innerText = summary;
+}
+
+// Initialize all functionality when the document is ready
+document.addEventListener("DOMContentLoaded", function () {
+  fetchAlbums();
+  setShipmentDateAttributes();
+  updateTotalPrice();
+  handleFormSubmission();
+});
